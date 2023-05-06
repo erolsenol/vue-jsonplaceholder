91d00783aa65f9e2fc01e73653c760f8924bc6e7
erolsenol <erol.senol@outlook.com>
2023-05-06 16:33:52 +0300

    vuetify added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store).use(vuetify).mount("#app");
